fcdbddee02356e3a921463893fa46468682fa716
metamn <bartus.csongor@gmail.com>
2017-09-10 13:51:23 +0300

    Working on the about

--- code/project/artwork/artwork.js
+++ code/project/artwork/artwork.js
@@ -1,1 +1,18 @@
 require('./../../framework/design/decorations/wave/wave.js');
+
+
+var click = require('./../../framework/helpers/js/click.js');
+var select = require('./../../framework/helpers/js/select.js');
+var klass = require('./../../framework/helpers/js/klass.js');
+
+
+var artwork = function() {
+  var trigger = select('.artwork');
+  var target = select('.about');
+
+  click(trigger, function() {
+    klass(target[0], 'about--active', 'toggle');
+  });
+}
+
+artwork();
